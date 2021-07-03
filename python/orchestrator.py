4292e1a62a82a6d22b914eae19f251f0f95c96a8
Hyungsoo Kim <javaos74@gmail.com>
2021-07-03 20:27:45 +0900

    X-UIPATH-OrganizationUnitId 항목 추가

--- python/orchestrator.py
+++ python/orchestrator.py
@@ -1,23 +1,48 @@
 import requests
+import json
 
 class Orchestrator:
-	token = None
 
-	def __init__(self, tenant, user, password, url = 'https://platform.uipath.com/'):
-		self.url = url
+	def __init__(self, tenant, user, password, url = 'https://uipath.myrobots.co.kr/'):
+		if url.endswith('/'):
+			self.url = url[:-1]
+		else:
+			self.url = url
+		self.headers = {'Authorization': 'Bearer ', 'Content-Type': 'application/json'}
 		self.token = self.__getToken(tenant, user, password)
+		if self.token:
+			self.headers["Authorization"] = "Bearer {0}".format(self.token)
 
 
-	def __getToken(cls, tenant, user, password):
-		res = cls.request('POST', 'api/account/authenticate', {'tenancyName': tenant,
+	def __getToken(self, tenant, user, password):
+		res = self.request('POST', '/api/account/authenticate', {'tenancyName': tenant,
                                                                        'usernameOrEmailAddress': user, 
                                                                        'password': password})
 		return res["result"]
 
+	def __getFolderId(self, folder_name):
+		response = requests.request( 'GET', self.url + "/odata/Folders?%24filter=DisplayName%20eq%20'{0}'&%24select=Id".format( folder_name), data=None, headers=self.headers)
+		if response.status_code == 200:
+			folder = response.json()
+			return str(folder["value"][0]["Id"])
+		else:
+			return None
 
-	def request(self, type, extension, body = None):
+
+	def add_queue_item(self, folder_name, body):
+		fid = self.__getFolderId(folder_name)
+		self.headers['X-UIPATH-OrganizationUnitId'] = fid
+		uri = self.url + '/odata/Queues/UiPathODataSvc.AddQueueItem'
+		response = requests.request('POST', uri, data=json.dumps(body,ensure_ascii=True), headers=self.headers)
+		if response.status_code in [200,201]:
+			return response.json()
+		else:
+			return None
+  	
+	def request(self, type, extension, body = None, folder = None):
 		uri = self.url + extension
-		headers = {'Authorization': 'Bearer ' + (self.token or '')}
-
-		response = requests.request(type.upper(), uri, data=body, headers=headers)
+		if folder:
+			fid = self.__getFolderId(folder)
+			self.headers['X-UIPATH-OrganizationUnitId'] = fid
+		response = requests.request(type.upper(), uri, data = json.dumps(body) if body else None, headers=self.headers)
 		return response.json()
--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 from orchestrator import Orchestrator
-import uuid
+import json
 
 #________________________________________________________________#
 #                    CREATE ORCHESTRATOR OBJECT                  #
@@ -7,20 +7,37 @@ import uuid
 #     Params: tenant, username, password, [orchestrator url]     #
 #________________________________________________________________#
 
-orch = Orchestrator("tenant", "username", "password")
+orch = Orchestrator("charles", "42maru", "수정한 패스워드 ", "https://uipath.myrobots.co.kr")
 
 #________________________________________________________________#
 #                          SEND REQUEST                          #
 #          Params: request type, url extension, [data]           #
 #________________________________________________________________#
 
-# GET
-res = orch.request('get', 'odata/Environments')
-print(res)
+entities = {
+	"name" : "SK텔레콤",
+	"date": "2021-07-03",
+	"variable": "다른 변수의 값",
+	"items": {
+		"변수1": "값1",
+		"변수2" : "값2"
+	}
+}
 
+entity_json_str = json.dumps( entities, ensure_ascii=False)
+print(entity_json_str)
 # POST
-res = orch.request('post', 'odata/Assets', {'Name': "Asset " + str(uuid.uuid4())[0:8],
-					    'ValueScope': "Global",
-					    'ValueType': "Text",
-  					    'StringValue': "Et tu asset 2"})
+res = orch.add_queue_item( "42maru", {
+  "itemData": {
+    "Name": "chatbot_42maru",
+    "Priority": "High",
+    "SpecificContent": { 
+		"intent": "my intent",
+		"entities": entity_json_str,
+		"user_id": "hyungsoo.kim@uipath.com",
+		"session_id": "x72llk23423xxxxs"
+        },
+    "Reference": "Intent Name"
+  }
+})
 print(res)
